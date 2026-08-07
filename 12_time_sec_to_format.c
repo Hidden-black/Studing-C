99884e93a2108f69983195287b82d72868b718e0
Shashwat Rai <68143131+Hidden-black@users.noreply.github.com>
2026-08-07 14:03:23 +0530

    Add time conversion from seconds to formatted output
    Implement a program to convert seconds into hours, minutes, and seconds.

---
+++ 12_time_sec_to_format.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*Function to take a input in sec and 
+covert it into hrs, min and sec
+*/
+int main(void){
+
+    int time, div=3600,tsec;
+    float thrs, tmin;
+
+    printf("Enter the time in sec: ");
+    scanf("%d", &time);
+
+    thrs= 0;
+    tmin= time/60;
+
+    while(tmin>59){
+        tmin = tmin-60;
+        thrs++;
+        printf("%2.f hrs", thrs);
+    }
+
+    printf("%2.f min",tmin);
+    printf(" %d sec", time%60); 
+
+    return 0;
+}
