36f1dd11e8dd0edcab4cdbe6a150148a877e3fac
Shashwat Rai <shashwatrai_28@outlook.com>
2026-08-13 11:54:44 +0530

    eeee

--- 10.c
+++ 10.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
- 
-int main(){
 
-    int num,mod,divider=2;
+int main() {
+
+    int num, mod, divider = 2;
     printf("Enter the Number: ");
     scanf("%d", &num);
 
-    if(num<2){
+    if (num < 2) {
         printf("The Number is not Prime");
-    } else{
-        while(num>divider){
-            mod= num%divider;
+    } else {
+        while (num > divider) {
+            mod = num % divider;
             divider++;
-            if(mod==0){
+            if (mod == 0) {
                 printf("The Number Is Not Prime\n");
                 return 0;
             }
---
+++ 17.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    int varA = 20;
+    printf("%d %p\n", varA, &varA);
+    return 0;
+}
