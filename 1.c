754fb366db5735ea27135aada3f1d456bbc8db7a
Shashwat Rai <shashwatrai_28@outlook.com>
2026-08-18 15:25:27 +0530

    e

--- 1.c
+++ 1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-  int var1, var2, var3;
-  var1 = 5;
-  var2 = 10;
-  var3 = var1 + var2;
-  printf("Var3: %d \n", var3);
-  printf return 0;
+    int var1, var2, var3;
+    var1 = 5;
+    var2 = 10;
+    var3 = var1 + var2;
+    printf("Var3: %d \n", var3);
+    return 0;
 }
